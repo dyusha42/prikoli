c24205f96cea5ac3a39784e083a4e04b0bfb2bfd
dyusha42 <jjakekin@gmail.com>
2025-04-06 18:14:03 +0700

    добавлен main() с сортировкой массива

--- lab1/sort.c
+++ lab1/sort.c
@@ -1,4 +1,5 @@
-// реализация пузырьковой сортировки
+#include <stdio.h>
+
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n-1; i++)
         for (int j = 0; j < n-i-1; j++)
@@ -7,4 +8,18 @@ void bubbleSort(int arr[], int n) {
                arr[j] = arr[j+1];
                arr[j+1] = temp;
             }
+}
+
+int main() {
+    int a[] = {4, 2};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    bubbleSort(a, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
